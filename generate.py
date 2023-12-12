77f10735b615c467bb3b80f54f114ba687653c17
Markus Enzweiler <markus.enzweiler@hs-esslingen.de>
2023-12-12 22:45:57 +0100

    fixed generate

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ torch.manual_seed(1337)
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
 # read it in to inspect it
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('german_presidents.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # Tokenizer
